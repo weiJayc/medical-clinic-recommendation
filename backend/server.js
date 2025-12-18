f187b236428bd184aefe13a7029f72d151840ea6
Amber <eric100060@gmail.com>
2025-12-18 18:43:21 +0800

    刪除多餘註解

--- backend/server.js
+++ backend/server.js
@@ -2,7 +2,6 @@ import express from "express";
 import dotenv from "dotenv";
 
 import recommendRoute from "./routes/recommend.js";
-//import hospitalsRoute from "./utils/findHospitals.js";
 
 dotenv.config();
 const app = express();
@@ -11,7 +10,6 @@ app.use(express.json());
 
 // API 路由
 app.use("/api/recommend", recommendRoute);
-//app.use("/api/hospitals", hospitalsRoute);
 
 app.listen(3001, () => {
   console.log("Server running: http://localhost:3001");
